760fc29bcb801c58a5c2c8d6b7d28b089825030f
Marcos Furlan <123518847+furlan-marcos@users.noreply.github.com>
2023-05-22 20:30:09 -0300

    primeiros exercicios

---
+++ unidade01/ex002/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main() {
+	int *ptr;
+	printf ("Endereco: %p\n\n", ptr);
+	ptr = (int *) malloc(sizeof (int));
+	printf ("Endereco: %p \nValor: %d\n\n", ptr, *ptr);
+	*ptr = 10;
+	printf ("Endereco: %p \nValor: %d\n\n", ptr, *ptr);
+	
+	int x;
+	x = 20;
+	ptr = &x;
+	printf ("Endereco: %p \nValor: %d\n\n", ptr, *ptr);
+	
+	ptr = (int *) malloc(sizeof (int));
+	printf ("Endereco: %p \nValor: %d\n\n", ptr, *ptr);
+	
+	system("Pause");
+	return(0);
+}
